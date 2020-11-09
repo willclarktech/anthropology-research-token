79abb77afa3c096f0a40477e13fc9a1b8de43667
willclarktech <willclarktech@users.noreply.github.com>
2020-11-09 17:22:47 +0100

    Format code

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,4 +1,6 @@
-const AnthropologyResearchToken = artifacts.require("AnthropologyResearchToken");
+const AnthropologyResearchToken = artifacts.require(
+	"AnthropologyResearchToken",
+);
 
 module.exports = async function (deployer) {
 	await deployer.deploy(AnthropologyResearchToken);
