f6b14a41068cee8c0d474399eb6b00936f7e3a9f
willclarktech <willclarktech@users.noreply.github.com>
2020-11-09 14:52:28 +0100

    Add migrations

---
+++ migrations/2_deploy.js
@@ -0,0 +1,5 @@
+const AnthropologyResearchToken = artifacts.require("AnthropologyResearchToken");
+
+module.exports = async function (deployer) {
+	await deployer.deploy(AnthropologyResearchToken);
+};
